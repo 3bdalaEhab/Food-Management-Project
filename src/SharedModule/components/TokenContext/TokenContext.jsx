0fdad1c77cc9202f8443ab0d28180003da9cd5ff
Abdala Ehab <127322606+3bdalaEhab@users.noreply.github.com>
2024-03-31 17:47:19 +0200

    Update TokenContext.jsx

--- src/SharedModule/components/TokenContext/TokenContext.jsx
+++ src/SharedModule/components/TokenContext/TokenContext.jsx
@@ -9,23 +9,6 @@ export default function TokenContextProvider({ children }) {
   
   const [Token, setToken] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  // const [JwtToken, setJwtToken] = useState({});
-
-//  useEffect(() => {
-//   jwtToken()
-//  }, [])
- 
-
-  // let jwtToken = ()=> {
-  //   let token = localStorage.getItem("token");
-  //   if (token) {
-  //     let jwtTok = jwtDecode(token);
-  //     setJwtToken(jwtTok);
-  //   } else {
-  //     setJwtToken({});
-  //   }
-  // }
-
 
   async function collLogin(values) {
     console.log(values);
@@ -39,7 +22,7 @@ export default function TokenContextProvider({ children }) {
 
     } catch (error) {
       console.log(error);
-      toast.error(error?.response?.data?.message, { duration: 800 });
+      toast.error(error?.response?.data?.message|| "There's a mistake.",{ duration: 800 });
       setIsLoading(false);
     }
   }
