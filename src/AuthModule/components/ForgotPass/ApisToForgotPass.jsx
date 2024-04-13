780eb00690532e2c415a7a527ef2e6c1af2870b6
Abdala Ehab <127322606+3bdalaEhab@users.noreply.github.com>
2024-04-13 23:54:58 +0200

    Update ApisToForgotPass.jsx

--- src/AuthModule/components/ForgotPass/ApisToForgotPass.jsx
+++ src/AuthModule/components/ForgotPass/ApisToForgotPass.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function ForgotPassEmail(values) {
     try {
-        let { data } = await axios.post("https://upskilling-egypt.com:443/api/v1/Users/Reset/Request", values);
+        let { data } = await axios.post("https://upskilling-egypt.com:3006/api/v1/Users/Reset/Request", values);
         return data
     } catch (error) {
         console.log(error);
